b93de2d1a23e5fc8348d3b2510393414212f5b45
danielaiv <voicuioanadaniela@yahoo.com>
2025-01-28 15:21:52 +0100

    xp bar

--- Project-Startup/Assets/VariableToTMP.cs
+++ Project-Startup/Assets/VariableToTMP.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VariableToTMP : MonoBehaviour
 {
@@ -11,19 +12,26 @@ public class VariableToTMP : MonoBehaviour
         COINS
     }
     [SerializeField]Display display;
+    [SerializeField] Slider slider;
     // Update is called once per frame
     void Update()
     {
         switch (display)
         {
             case Display.LVL:
-                text.text = GameManager.instance.lvl.ToString();
+               if(text!=null) text.text = GameManager.instance.lvl.ToString();
                 break;
             case Display.XP:
-                text.text = (GameManager.instance.exp / GameManager.instance.expToNextLevel).ToString(); 
+                if (slider != null)
+                {
+                    slider.maxValue = GameManager.instance.expToNextLevel;
+                    slider.value = GameManager.instance.exp;
+                    print((GameManager.instance.exp / GameManager.instance.expToNextLevel));
+
+                }
                 break;
             case Display.COINS:
-                text.text= GameManager.instance.coins.ToString();
+                if (text != null) text.text= GameManager.instance.coins.ToString();
                 break;
         }
     }
